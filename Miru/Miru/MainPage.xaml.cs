ce2bf1799708bcfa86ec7c10da3425e977add6c7
sepi <deet1022@gmail.com>
2016-06-13 00:37:51 +0900

    Add weather view

--- Miru/Miru/MainPage.xaml.cs
+++ Miru/Miru/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,8 +14,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// 빈 페이지 항목 템플릿에 대한 설명은 http://go.microsoft.com/fwlink/?LinkId=234238 에 나와 있습니다.
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace Miru
 {
@@ -23,6 +25,7 @@ namespace Miru
 	public sealed partial class MainPage : Page
 	{
 		DispatcherTimer m_dispatcherTimer;
+		Weather WeatherInfo;
 
 		public MainPage()
 		{
@@ -30,19 +33,25 @@ namespace Miru
 			Loaded += MainPage_Loaded;
 			Unloaded += MainPage_Unloaded;
 		}
-		private void MainPage_Loaded(object sender, RoutedEventArgs e)
+
+		private async void MainPage_Loaded(object sender, RoutedEventArgs e)
 		{
 			m_dispatcherTimer = new DispatcherTimer();
 			m_dispatcherTimer.Tick += M_dispatcherTimer_Tick;
 			m_dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
 			m_dispatcherTimer.Start();
+
+			string Json = await Request_WeatherJson();
+			WeatherInfo = JsonToWeather(Json);
+
+			Weather_Temp.Text = $"{WeatherInfo.Temperature}℃";
 		}
 
 		private void M_dispatcherTimer_Tick(object sender, object e)
 		{
 			DateTime now = DateTime.Now;
 
-			
+
 
 			int Hour_ = (now.Hour > 12) ? now.Hour - 12 : now.Hour;
 			string Hour = (Hour_ < 10) ? $"0{Hour_}" : Hour_.ToString();
@@ -98,5 +107,60 @@ namespace Miru
 			}
 			return Week;
 		}
+
+		private async Task<string> Request_WeatherJson()
+		{
+			// 날씨 API 제공: https://developers.skplanetx.com/apidoc/kor/weather/
+
+			// 현재날씨(분별)
+			string url = "http://apis.skplanetx.com/weather/current/minutely?";
+			// API 버전 정보
+			int version = 1;
+			// 경위도
+			double lat = 37.285944;
+			double lon = 127.636764;
+			// 앱키
+			string appkey = "5424eae1-8e98-3d89-82e5-e9a1c589a7ba";
+
+			url = $"{url}version={version}&lat={lat}&lon={lon}&appKey={appkey}";
+
+			HttpClient client = new HttpClient();
+			Task<string> getStringTask = client.GetStringAsync(url);
+			string result = await getStringTask;
+
+			return result;
+		}
+
+		private Weather JsonToWeather(string Json)
+		{
+			Weather weather = new Weather { Temperature = 0, Humidiy = 0, SkyCode = null };
+
+			try
+			{
+				JObject obj = JObject.Parse(Json);
+				JArray arr = JArray.Parse(obj["weather"]["minutely"].ToString());
+
+				foreach(var item in arr)
+				{
+					weather.Temperature = Convert.ToDouble(item["temperature"]["tc"].ToString());
+					weather.Humidiy = Convert.ToDouble(item["humidity"].ToString());
+					weather.SkyCode = item["sky"]["code"].ToString();
+				}
+			}
+			catch(Exception)
+			{
+				throw;
+			}
+
+			return weather;
+		}
+
+	}
+
+	class Weather
+	{
+		public double Temperature { get; set; }
+		public double Humidiy { get; set; }
+		public string SkyCode { get; set; }
 	}
 }
