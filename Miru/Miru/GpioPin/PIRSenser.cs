4e32484bac3ed8fc69f0d2960464386034611be1
sepi <deet1022@gmail.com>
2016-07-29 20:18:03 +0900

    Add GetPinValue Property

--- Miru/Miru/GpioPin/PIRSenser.cs
+++ Miru/Miru/GpioPin/PIRSenser.cs
@@ -4,16 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
+using System.Threading;
 
 namespace Miru.GpioPin
 {
-    public class PIRSenser : Senser
+    public class PIRSenser : Senser, IDisposable
     {
         private const int PIR_SENSER_PIN = 23;
         private Windows.Devices.Gpio.GpioPin pin;
+        private GpioPinValue _pinValue;
+        public GpioPinValue pinValue
+        {
+            get
+            {
+                this._pinValue = pin.Read();
+                if (this._pinValue == GpioPinValue.High)
+                {
+                    return GpioPinValue.High;
+                }
+                return GpioPinValue.Low;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.pin.Dispose();
+        }
+
 
         /// <summary>
-        /// PIRSenser를 읽어들입니다. 실패했을시 false를 반환합니다.
+        /// PIRSenser를 읽어들입니다. 실패시 false를 반환합니다.
         /// </summary>
         /// <returns></returns>
         public override bool InitGpio()
--- Miru/Miru/MainPage.xaml.cs
+++ Miru/Miru/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.Devices.Gpio;
 using Miru.GpioPin;
+using System.Threading.Tasks;
 
 namespace Miru
 {
@@ -13,7 +14,9 @@ namespace Miru
         private DispatcherTimer timer;
         private Clock clock;
         private WeatherWidget weather;
-        private PIRSenser pirSenser = new PIRSenser();
+        private PIRSenser pirSenser;
+        private int count;
+        private bool isSetView;
 
         ResourceLoader rl = new ResourceLoader();
 
@@ -22,6 +25,12 @@ namespace Miru
             InitializeComponent();
             Loaded += MainPage_Loaded;
             Unloaded += MainPage_Unloaded;
+            this.Opacity = 0;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.pirSenser = new PIRSenser();
 
             bool isSuccessed = pirSenser.InitGpio();
 
@@ -33,15 +42,19 @@ namespace Miru
             {
                 this.status.Text = rl.GetString("pin_error");
             }
-        }
-
-        private void MainPage_Loaded(object sender, RoutedEventArgs e)
-        {
-            this.clock = new Clock();
             this.timer = new DispatcherTimer();
             this.timer.Tick += M_Clock_Tick;
             this.timer.Interval = TimeSpan.FromSeconds(1);
+            this.timer.Start();
 
+            SetView();
+        }
+
+        private void SetView()
+        {
+            isSetView = true;
+
+            this.clock = new Clock();
             CreateWidget();
             switch (this.clock.TimeStatus)
             {
@@ -65,6 +78,7 @@ namespace Miru
                     Center.Text = rl.GetString("helloMiru_default");
                     break;
             }
+            this.Opacity = 1;
         }
 
         private async void CreateWidget()
@@ -72,31 +86,58 @@ namespace Miru
             this.weather = new WeatherWidget(1, 37.285944, 127.636764, "5424eae1-8e98-3d89-82e5-e9a1c589a7ba");
             await this.weather.RequestWeatherAsync();
 
-            this.timer.Start();
             currentWeatherTemp.Text = $"{this.weather.Temperatures?[0]}℃";
             currentWeatherIcon.Text = this.weather.WeatherIcons?[0].ToString();
-
-            
-
         }
 
         private void M_Clock_Tick(object sender, object e)
         {
-            clockState.Text = clock.GetCurrentState();
-            clockTime.Text = clock.GetCurrentTime();
-            clockDate.Text = clock.getCurrentWeek();
+            count++;
+            if (! isSetView)
+            {
+                if (pirSenser.pinValue == GpioPinValue.High)
+                {
+                    count = 0;
+                    SetView();
+                }
+            }
+            else
+            {
+                if (count > 9)
+                {
+                    count = 0;
+                    CloseView();
+                }
+                else
+                {
+                    clockState.Text = clock.GetCurrentState();
+                    clockTime.Text = clock.GetCurrentTime();
+                    clockDate.Text = clock.getCurrentWeek();
+                }
+            }
+        }
+        private void CloseView()
+        {
+            /*
+            this.clock = null;
+            this.weather = null;
+            */
+            this.MainGrid.Opacity = 0;
+            this.isSetView = false;
         }
 
         private void MainPage_Unloaded(object sender, RoutedEventArgs e)
         {
-            if(timer != null)
+            if (timer != null)
             {
-                if(timer.IsEnabled)
+                if (timer.IsEnabled)
                 {
                     timer.Tick -= M_Clock_Tick;
                 }
             }
             timer.Stop();
+
+            this.pirSenser.Dispose();
         }
     }
 }
