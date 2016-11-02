bf36837036a5eab97b94b08e606659155c04eeb7
Sepi <deet1022@gmail.com>
2016-11-02 16:02:12 +0900

    주석 추가

--- Miru/Miru/Util/WeatherConfig.cs
+++ Miru/Miru/Util/WeatherConfig.cs
@@ -9,9 +9,16 @@ namespace Miru.Util
     public static class WeatherConfig
     {
         // weather api data
+
         public static readonly int version = 1;
+
+        //대한민국 경기도 여주시 홍문동
         public static readonly double lat = 37.285944;
         public static readonly double lon = 127.636764;
+
+        /// <summary>
+        /// Miru의 appKey
+        /// </summary>
         public static readonly string appKey = "5424eae1-8e98-3d89-82e5-e9a1c589a7ba";
     }
 }
