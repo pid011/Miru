d706be0b08947fc66288a654e3094eaec24407e9
Sepi <deet1022@gmail.com>
2017-07-09 16:56:55 +0900

    유틸 추가

---
+++ Miru/Miru/Utils/MiruUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miru.Utils
+{
+    public class MiruUtil
+    {
+        public static string ConvertString(int target) => target < 10 ? $"0{target}" : target.ToString();
+    }
+}
