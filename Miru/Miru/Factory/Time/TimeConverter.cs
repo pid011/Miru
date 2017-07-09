b95c57d0b6c0871dd8ea6f9c6ed81e298c40dfbe
Sepi <deet1022@gmail.com>
2017-07-09 16:55:55 +0900

    오류 수정

---
+++ Miru/Miru/Factory/Time/TimeConverter.cs
@@ -0,0 +1,72 @@
+﻿using Miru.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Miru.Factory.Time.TimeFactory;
+
+namespace Miru.Factory.Time
+{
+    public class TimeConverter
+    {
+        public static string ConvertString(AMPMS target)
+        {
+            string result = string.Empty;
+
+            switch (target)
+            {
+                case AMPMS.AM:
+                    result = StringManager.GetString("AM");
+                    break;
+
+                case AMPMS.PM:
+                    result = StringManager.GetString("PM");
+                    break;
+            }
+            return result;
+        }
+
+        public static string ConvertString(DayOfWeek target)
+        {
+            string result = string.Empty;
+
+            switch (target)
+            {
+                case DayOfWeek.Monday:
+                    result = StringManager.GetString("Mon");
+                    break;
+
+                case DayOfWeek.Tuesday:
+                    result = StringManager.GetString("Tue");
+                    break;
+
+                case DayOfWeek.Wednesday:
+                    result = StringManager.GetString("Wed");
+                    break;
+
+                case DayOfWeek.Thursday:
+                    result = StringManager.GetString("Thu");
+                    break;
+
+                case DayOfWeek.Friday:
+                    result = StringManager.GetString("Fri");
+                    break;
+
+                case DayOfWeek.Saturday:
+                    result = StringManager.GetString("Sat");
+                    break;
+
+                case DayOfWeek.Sunday:
+                    result = StringManager.GetString("Sun");
+                    break;
+            }
+            return result;
+        }
+
+        public static int ConvertHour(int hour)
+        {
+            return hour > 12 ? hour - 12 : hour == 0 ? 12 : hour;
+        }
+    }
+}
--- Miru/Miru/Factory/Time/TimeFactory.cs
+++ Miru/Miru/Factory/Time/TimeFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Miru.Utils;
+using System;
 using Windows.ApplicationModel.Resources;
 using Windows.Globalization.DateTimeFormatting;
 
@@ -22,64 +23,6 @@ namespace Miru.Factory.Time
 
         private AMPMS GetAMPM(DateTime date) => (date.Hour > 11) ? AMPMS.PM : AMPMS.AM;
 
-        private int ConvertHour(int hour) => hour > 12 ? hour - 12 : hour == 0 ? 12 : hour;
-
-        private string ConvertString(AMPMS target)
-        {
-            string result = string.Empty;
-
-            switch (target)
-            {
-                case AMPMS.AM:
-                    result = loader.GetString("AM");
-                    break;
-
-                case AMPMS.PM:
-                    result = loader.GetString("PM");
-                    break;
-            }
-            return result;
-        }
-
-        private string ConvertString(DayOfWeek target)
-        {
-            string result = string.Empty;
-
-            switch (target)
-            {
-                case DayOfWeek.Monday:
-                    result = loader.GetString("Mon");
-                    break;
-
-                case DayOfWeek.Tuesday:
-                    result = loader.GetString("Tue");
-                    break;
-
-                case DayOfWeek.Wednesday:
-                    result = loader.GetString("Wed");
-                    break;
-
-                case DayOfWeek.Thursday:
-                    result = loader.GetString("Thu");
-                    break;
-
-                case DayOfWeek.Friday:
-                    result = loader.GetString("Fri");
-                    break;
-
-                case DayOfWeek.Saturday:
-                    result = loader.GetString("Sat");
-                    break;
-
-                case DayOfWeek.Sunday:
-                    result = loader.GetString("Sun");
-                    break;
-            }
-            return result;
-        }
-
-        private string ConvertString(int target) => target < 10 ? $"0{target}" : target.ToString();
-
         public class TimeViewItem
         {
             public string Date { get; }
@@ -94,8 +37,8 @@ namespace Miru.Factory.Time
 
                 var sdatefmt = new DateTimeFormatter("shortdate");
                 Date = sdatefmt.Format(time);
-                Time = $"{factory.ConvertString(factory.ConvertHour(time.Hour))}:{factory.ConvertString(time.Minute)}";
-                AMPM = factory.ConvertString(ampm);
+                Time = $"{MiruUtil.ConvertString(TimeConverter.ConvertHour(time.Hour))}:{MiruUtil.ConvertString(time.Minute)}";
+                AMPM = TimeConverter.ConvertString(ampm);
             }
         }
     }
