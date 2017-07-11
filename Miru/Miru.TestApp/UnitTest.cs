ba16767c7c961dc2e4d124a64642956582a6fa75
Sepi <deet1022@gmail.com>
2017-07-12 01:40:22 +0900

    oops

--- Miru/Miru.TestApp/UnitTest.cs
+++ Miru/Miru.TestApp/UnitTest.cs
@@ -13,10 +13,6 @@ namespace Miru.TestApp
         [TestMethod]
         public void TestMethod1()
         {
-            var weather = new WeatherFactory(71, 121, @"ZAPOSsSfE2OT%2F5L%2BOvgt9jjeNsTvt70ZX2uqpANGth%2B2%2BhGeA2RUx2dCXI3qEa3%2BCglP8AONokRPyfIkCET8zA%3D%3D");
-            var result = weather.GetCurrentWeatherItem();
-
-            Debug.Write("Completed!");
         }
     }
 }
