53fcbd2c823c797c63276fe2ba01eb697b20b511
Sepi <deet1022@gmail.com>
2017-07-12 01:32:26 +0900

    Create NewsItem class

---
+++ Miru/Miru/Factory/News/NewsItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miru.Factory.News
+{
+    public class NewsItem
+    {
+        public string Title { get; set; }
+        public string OriginalLink { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public DateTime PubDate { get; set; }
+    }
+}
